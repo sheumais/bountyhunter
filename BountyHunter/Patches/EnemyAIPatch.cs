57df9021cd554e8fc217832e354a00355a530360
sheumais <88064574+sheumais@users.noreply.github.com>
2023-11-29 16:14:11 +1100

    Add project files.

---
+++ BountyHunter/Patches/EnemyAIPatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BountyHunter.Patches
+{
+    [HarmonyPatch(typeof(EnemyAI))]
+    internal class EnemyAIPatch
+    {
+        [HarmonyPatch(nameof(EnemyAI.KillEnemy))]
+        [HarmonyPostfix]
+        static void rewardCreditsPatch()
+        {
+            Terminal terminal = UnityEngine.Object.FindObjectOfType<Terminal>();
+            terminal.useCreditsCooldown = true;
+            terminal.groupCredits += 1000;
+            terminal.SyncGroupCreditsServerRpc(terminal.groupCredits, terminal.numberOfItemsInDropship);
+            Debug.Log("Adding money");
+        }
+    }
+}
---
+++ BountyHunter/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using BountyHunter.Patches;
+using HarmonyLib;
+
+namespace BountyHunter
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class BountyHounterBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Sheumais.BountyHunter";
+        private const string modName = "Bounty Hunter";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static BountyHounterBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null) {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("Bounty Hunter has awoken.");
+
+            harmony.PatchAll(typeof(BountyHounterBase));
+            harmony.PatchAll(typeof(EnemyAIPatch));
+        }
+    }
+}
